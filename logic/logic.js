99258f20704bf76f68a577da6d80f3b407f742a0
Matias Gochez <gochezmatias@gmail.com>
2023-09-20 15:44:42 -0300

    (feat/logic): creacion de carpeta y archivo

---
+++ logic/logic.js
@@ -0,0 +1,26 @@
+const suma = (a , b) => {
+    return a + b;
+};
+
+
+const resta = (a , b) => {
+    return a - b;
+};
+
+
+const multiplicacion = (a , b) => {
+    return a * b;
+};
+
+const division = (a, b) => {
+    return a / b;
+};
+
+//hola
+
+export default {
+    suma,
+    resta,
+    multiplicacion,
+    division
+}
